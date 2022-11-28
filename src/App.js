64f3bbf80e5097a67bcd6240159b348a65bc3432
Chandra <lohitreddycht@gmail.com>
2022-11-27 22:57:41 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Shell from './Shell.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Shell>
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    component={Login}
+                  />
+                  <Route
+                    exact
+                    path="/login"
+                    component={Login}
+                  />
+                  <Route
+                    exact
+                    path="/register"
+                    component={Register}
+                  />
+                </Switch>
+            </Shell>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+import Button from '@mui/material/Button';
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <h3>Pass<span>Management </span></h3>
+                {/* <Button variant="contained" size="medium">Login   </Button> */}
+                
+            </div>
+        </>
+    );
+}
+
+export default Header;
